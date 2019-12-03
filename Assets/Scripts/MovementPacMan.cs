5fe2fce58094b116bf3a8235f4e632e16dd75f9b
Simone Antonacci <simo.antonacci@gmail.com>
2019-12-03 19:02:14 +0100

    Update MovementPacMan.cs

--- Assets/Scripts/MovementPacMan.cs
+++ Assets/Scripts/MovementPacMan.cs
@@ -9,27 +9,30 @@ public class MovementPacMan : MonoBehaviour
     const int DOWN_DIRECTION = -90;
     const int LEFT_DIRECTION = -180;
     const int RIGHT_DIRECTION = 0;
-
+    
+    Vector2 NextDirection;
+    Collider2D pacManCollider;
     Vector2 Direction = Vector2.zero;
 
+    public float PacManSpeed;
     //Muove PacMan in base al vettore passato come parametro
-    void _move(Vector2 direction)
+    void Move(Vector2 direction)
     {
         if (direction == Vector2.up)
-            _rotate(UP_DIRECTION);
+            Rotate(UP_DIRECTION);
         else if (direction == Vector2.down)
-            _rotate(DOWN_DIRECTION);
+            Rotate(DOWN_DIRECTION);
         else if (direction == Vector2.left)
-            _rotate(LEFT_DIRECTION);
+            Rotate(LEFT_DIRECTION);
         else if (direction == Vector2.right)
-            _rotate(RIGHT_DIRECTION);
+            Rotate(RIGHT_DIRECTION);
 
         transform.localPosition += (Vector3)(direction * PacManSpeed) * Time.deltaTime;
     }
 
     //Ottiene l'input da (WASD o freccette) e restituisce il vettore in cui PacMan dovrebbe
     //svoltare, o un vettore nullo se non viene dato alcun input
-    Vector2 _getInput()
+    Vector2 GetInput()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
             return Vector2.left;
@@ -43,12 +46,12 @@ public class MovementPacMan : MonoBehaviour
         else return Vector2.zero;
     }
 
-    void _rotate(int rotation)
+    void Rotate(int rotation)
     {
         transform.localRotation = Quaternion.Euler(0, 0, rotation);
     }
 
-    bool _isValid(Vector2 direction)
+    bool IsValid(Vector2 direction)
     {
         Vector2 pos = transform.position;
         Vector2 linecastVector = Vector2.zero;
@@ -63,81 +66,31 @@ public class MovementPacMan : MonoBehaviour
         else return false;
 
         var hit = Physics2D.Linecast(linecastVector, pos);
-        if (hit.collider == _pacManCollider)
+        if (hit.collider == pacManCollider)
             return true;
         else return false;
     }
 
 
-    void _update()
-    {
-        var input = _getInput();
-        if (input != Vector2.zero)
-        {
-            _nextDirection = input;
-        }
-
-        if (_nextDirection != Vector2.zero && _isValid(_nextDirection))
-        {
-            Direction = _nextDirection;
-            _nextDirection = Vector2.zero;
-        }
-        _move(Direction);
-    }
-
-
-
-
-    //CODICE VECCHIO
-    Rigidbody2D pacManRB;
-    Collider2D _pacManCollider;
-    public float PacManSpeed;
-    public Vector2 direzione = Vector2.zero;
-    public Vector2 _nextDirection;
     void Start()
     {
-        pacManRB = GetComponent<Rigidbody2D>();
-        _pacManCollider = GetComponent<Collider2D>();
-        _nextDirection = Vector2.left;
+        pacManCollider = GetComponent<Collider2D>();
+        NextDirection = Vector2.left;
     }
 
     void Update()
     {
-        //GetInput();
-        _update();
-    }
+        var input = GetInput();
+        if (input != Vector2.zero)
+        {
+            NextDirection = input;
+        }
 
-
-    /*Ottiene l'input (freccette o WASD), cambia la direzione del vettore a cui
-         * punta Pac-Man e muove lo sprite*/
-    void GetInput()
-    {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        if (NextDirection != Vector2.zero && IsValid(NextDirection))
         {
-            direzione = Vector2.left;
-            transform.localRotation = Quaternion.Euler(0, 0, -180);
+            Direction = NextDirection;
+            NextDirection = Vector2.zero;
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-        {
-            direzione = Vector2.right;
-            transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-        {
-            direzione = Vector2.up;
-            transform.localRotation = Quaternion.Euler(0, 0, 90);
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-        {
-            direzione = Vector2.down;
-            transform.localRotation = Quaternion.Euler(0, 0, -90);
-        }
-        Move();
-    }
-
-    //Modifica la posizione di pacman in base alla direzione del vettore
-    void Move()
-    {
-        transform.localPosition += (Vector3)(direzione * PacManSpeed) * Time.deltaTime;
+        Move(Direction);
     }
 }
