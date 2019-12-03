fa37b081454d164a2df9be3d859a315214df60db
Kevin-A02 <Kevin-A02@users.noreply.github.com>
2019-12-03 22:42:22 +0100

    Score su schermo aggiunto

--- Assets/Scripts/PacManFood.cs
+++ Assets/Scripts/PacManFood.cs
@@ -1,33 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PacManFood : MonoBehaviour
 {
-    public bool isFood;
     int score;
-
+    public Text ScoreText;
     public GameObject Player;
     public GameObject Food;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        
-    }
     void OnTriggerEnter2D(Collider2D other)
     {             
          if (other.gameObject.tag == "PacManfood")
          {
               score = score + 1;
               Debug.Log("Score = " + score);
+              ScoreText.text= "Score = " + score.ToString();
          }      
      }
 }
