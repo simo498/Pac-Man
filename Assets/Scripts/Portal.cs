e5ddd1562170cbbd4f4cc4bdaeafa9090cd43c84
Benny-glitch <58368441+Benny-glitch@users.noreply.github.com>
2019-12-03 18:09:33 +0100

    portale finito

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-
+    //dichiatazione variabili 
     private Transform destinazione;
     public bool isOrange;
-    float DistMin = 0.3f;
-    // Start is called before the first frame update
+    float DistMin = 0.3f;    
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(isOrange == false)
+        //controllo portali e inserimento nelle varibili della sua posizione 
+        if (isOrange == false)
         {
             destinazione = GameObject.FindGameObjectWithTag("orange").GetComponent<Transform>();
         }
@@ -27,12 +22,16 @@ public class Portal : MonoBehaviour
             destinazione = GameObject.FindGameObjectWithTag("blue").GetComponent<Transform>();
         }
     }
+    void Update()
+    { }
+    //metodo che riconosce la collisione tra il player e il portale 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(Vector2.Distance(transform.position,other.transform.position) > DistMin && (other.gameObject.tag == "Player") )
-        { 
-            other.transform.position = new Vector2(destinazione.position.x, destinazione.position.y);         
-        }
-        
+       if(Vector2.Distance(transform.position, other.transform.position) > 0.1f)
+       {
+            other.transform.position = new Vector2(destinazione.position.x, destinazione.position.y);
+       }                       
     }
+    
+
 }
