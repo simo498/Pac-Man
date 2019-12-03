bb46c2bc246aa4ba045065aea185dae5645c06d0
Simone Antonacci <simo.antonacci@gmail.com>
2019-12-03 15:57:10 +0100

    Migliorato il sistema di rilevamento dei collider

--- Assets/Scripts/MovementPacMan.cs
+++ Assets/Scripts/MovementPacMan.cs
@@ -10,9 +10,6 @@ public class MovementPacMan : MonoBehaviour
     const int LEFT_DIRECTION = -180;
     const int RIGHT_DIRECTION = 0;
 
-    /*readonly Vector2[] UP_DOWN_VECTOR = new Vector2[2] { Vector2.left, Vector2.right };
-    readonly Vector2[] LEFT_RIGHT_VECTOR = new Vector2[2] { Vector2.up, Vector2.down };*/
-
     Vector2 Direction = Vector2.zero;
 
     //Muove PacMan in base al vettore passato come parametro
@@ -53,8 +50,19 @@ public class MovementPacMan : MonoBehaviour
 
     bool _isValid(Vector2 direction)
     {
-        var hit = Physics2D.Linecast((Vector2)transform.position + direction,
-                                    (Vector2)transform.position);
+        Vector2 pos = transform.position;
+        Vector2 linecastVector = Vector2.zero;
+        if (direction == Vector2.left)
+            linecastVector = pos + new Vector2(-0.4f, 0.0f);
+        else if (direction == Vector2.right)
+            linecastVector = pos + new Vector2(0.4f, 0.0f);
+        else if (direction == Vector2.up)
+            linecastVector = pos + new Vector2(0.0f, 0.4f);
+        else if (direction == Vector2.down)
+            linecastVector = pos + new Vector2(0.0f, -0.4f);
+        else return false;
+
+        var hit = Physics2D.Linecast(linecastVector, pos);
         if (hit.collider == _pacManCollider)
             return true;
         else return false;
