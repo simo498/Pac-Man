fef047556d0fd6040a1b1d522ff858a765142b0f
Simone Antonacci <simo.antonacci@gmail.com>
2019-12-04 17:39:03 +0100

    Miglioramenti generali

--- Assets/Scripts/MovementPacMan.cs
+++ Assets/Scripts/MovementPacMan.cs
@@ -67,10 +67,9 @@ public class MovementPacMan : MonoBehaviour
         else return false;
 
         var hit = Physics2D.Linecast(linecastVector, pos);
-        if (hit.collider == pacManCollider || hit.collider.tag == "PacManfood" ||
-            hit.collider.tag == "PacManSfood")
-            return true;
-        else return false;
+        if (hit.collider.CompareTag("Wall"))
+            return false;
+        else return true;
     }
 
 
